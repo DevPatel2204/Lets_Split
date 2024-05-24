cc6553ca97e6f9ea2617eb88a8afdf2594a5a69c
Dev Patel <dpatel222430@gmail.com>
2024-05-25 00:08:11 +0530

    Initial Commit

---
+++ Lets_Split/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lets_Split
+//
+//  Created by Dev Patel on 25/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lets_Split/Lets_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lets_SplitApp.swift
+//  Lets_Split
+//
+//  Created by Dev Patel on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Lets_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
