e2a9f4242520665616e0ade934fc06d28792a383
Dev Patel <dpatel222430@gmail.com>
2024-05-25 02:28:11 +0530

    Bug:preview crash . Working on it

--- Lets_Split/ContentView.swift
+++ Lets_Split/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var totalCost = ""
+    @State private var people :Int = 2
+    @State private var tipIdex :Int = 4
+    let tippercentages = [5,10,15,20]
+    
+    func calculateTotal() -> Double {
+        let tip = Double(tippercentages[tipIdex])
+        let ordertotal = Double(totalCost) ?? 0
+        let finalamount = ((ordertotal/100 * tip)+ordertotal)
+        
+        return finalamount/Double(people)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            Form {
+                Section(header:Text("Please enter the amount")){
+                    TextField("Amount",text: $totalCost).keyboardType(.decimalPad)
+                }
+                Section(header:Text("Please select a tip amount")){
+                    Picker("Tip percent",selection: $tipIdex){
+                        ForEach(0..<tippercentages.count){
+                            Text("\(tippercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header:Text("How many Peopple?")){
+                    Picker("How many People",selection: $people){
+                        ForEach(0 ..< 25){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header:Text("Total per person")){
+                    Text("RS\(calculateTotal(),specifier: "$2f")")
+                }
+            }.navigationTitle("Let's Split It Up")
         }
-        .padding()
+
     }
 }
 
